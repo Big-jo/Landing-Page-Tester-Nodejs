513a8911ec5bf9b51f7a0091aedbbaf75ce61797
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-09 17:00:21 +0100

    core: setup mongoose connection and configure dotenv

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let dotenv = require('dotenv');
+let connectDB = require('./config/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+dotenv.config({ path: './config/config.env' });
 
+connectDB();
 var app = express();
 
 app.use(logger('dev'));
