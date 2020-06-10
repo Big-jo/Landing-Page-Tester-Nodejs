58c0343f40b4ff65d28cdc9617af0ca6b4ed50dd
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-10 14:11:38 +0100

    configured awis with sample code

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let dotenv = require('dotenv');
-let connectDB = require('./config/db');
+var awis = require('awis');
 
+let connectDB = require('./config/db');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 dotenv.config({ path: './config/config.env' });
 
 connectDB();
 var app = express();
+var client = awis({
+  key: process.env.AWSACCESSKEYID,
+  secret: process.env.AWSSECRETACCESSKEY,
+});
+
+client(
+  {
+    Action: 'UrlInfo',
+    Url: 'foo.com',
+    ResponseGroup: 'Related,TrafficData,ContentData',
+  },
+  function (err, data) {
+    // ...
+  }
+);
 
 app.use(logger('dev'));
 app.use(express.json());
